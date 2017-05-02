7de9fe8129f880cfa745ae9fd8c1ac421d75b8c0
Sasha Wootton <SashaWootton@Sashas-MacBook-Pro.local>
2017-05-01 20:35:43 -0400

    added images, header

--- index.php
+++ index.php
@@ -4,18 +4,19 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Church</title>
+    <title>First Presbyterian Church of Waterloo</title>
     <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> 
     <link rel="stylesheet" href="css/style.css" type="text/css"/>
     <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <style>@import 'https://fonts.googleapis.com/css?family=Lato|Merriweather';</style>
-	
   </head>
 <body>
+    <div class="header">
+        <img src="images/C_windows.jpeg" alt="This should be a picture of windows">
+    </div>
+     
     
-    YEAY! 
-    
-    </body>
+</body>
 
 </html>
