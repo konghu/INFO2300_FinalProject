2885efd73fbf48204a860683aa776f242e385c42
Sasha Wootton <SashaWootton@Sashas-MacBook-Pro.local>
2017-05-01 20:11:24 -0400

    added css file

--- index.php
+++ index.php
@@ -1,10 +1,21 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Kong
- * Date: 4/21/2017
- * Time: 12:51 PM
 
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Church</title>
+    <link rel="stylesheet" type="text/css" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css"> 
+    <link rel="stylesheet" href="css/style.css" type="text/css"/>
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <style>@import 'https://fonts.googleapis.com/css?family=Lato|Merriweather';</style>
+	
+  </head>
+<body>
+    
+    YEAY! 
+    
+    </body>
 
-Mahima - Test Commit
-
+</html>
