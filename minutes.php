5c2992eb14c25cc3c4e5d832f9f21c00fe242c59
ms11711 <shahmahima@gmail.com>
2017-05-15 22:54:54 -0400

    meeting minutes

--- minutes.php
+++ minutes.php
@@ -109,8 +109,8 @@ if(!isset($_POST['submit_logout'])){
 
         if (!empty($_FILES['newphoto']['name'])){
           $FileType = pathinfo($originalName, PATHINFO_EXTENSION);
-          if ($FileType != "txt" && $FileType != "doc" && $FileType != "docx"){
-            echo"The file type is not supported. (only supports .txt .doc or .docx) Minutes not uploaded.";
+           if ($FileType != "txt"){
+             echo"The file type is not supported. (only supports .txt) Minutes not uploaded.";
             echo"<br>";
             return NULL;
           }else if(file_exists($path)){
@@ -147,10 +147,7 @@ if(!isset($_POST['submit_logout'])){
    
     echo"Overview: $row[overview]<br>";
     echo"lecturer: $row[lecturer]<br>";
-    echo "$row[path]";
-    echo "$row[minuteID]";
-    echo("<a href='min.php?min_id=".$row['minuteID']."'>
-      < src='minute/".$row['path']."' alt='Meme'></a><br>");
+    echo("<a href='min.php?min_id=".$row['minuteID']."'>".$row['path']." </a><br>");
    
   }
   
