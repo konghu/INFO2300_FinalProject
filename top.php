7f9c829e90b096570ef43deeec5020e0241a3bc5
sashawootton <ajw279@cornell.edu>
2017-11-13 11:49:38 -0500

    added top
    ajfgsuydgafjh

---
+++ top.php
@@ -0,0 +1,15 @@
+
+
+    <div id= "top" > 
+        <div id="banner">
+            <h1>
+                <a href="add_entry.php">American Gymnastics Catalog</a>
+            </h1>
+        </div> <!--end banner-->
+        <div id="nav_bar">
+            <a href="https://www.youtube.com/watch?v=HyCGQMt129s" target="_blank"> "Seven days without gymnastics makes one weak" </a>
+            
+        </div > 
+    </div>  
+
+
